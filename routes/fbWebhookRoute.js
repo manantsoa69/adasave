f19e73b11ec24e6401852437b1afe3a855e31a4a
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2024-04-10 23:56:33 +0200

    Add files via upload

--- routes/fbWebhookRoute.js
+++ routes/fbWebhookRoute.js
@@ -1,84 +1,180 @@
+// routes/fbWebhookRoute.js
 const express = require('express');
-const axios = require('axios');
 const router = express.Router();
-const { getStoredNumbers, deleteDataFromRedis } = require('../redis');
-const { sendMessageA } = require('../helper/messengerAdmin');
-const { MongoClient } = require('mongodb'); // Import MongoDB module
+const { checkSubscription, check } = require('../helper/subscriptionHelper');
+const { sendMessage, selectLang, menuMesg, selectLangtrad,yesNo } = require('../helper/messengerApi');
+const { checkNumber } = require('./numberValidation');
+const { googlechat } = require('../public/googleApi');
+const { googlechat1 } = require('../public/googleApi1');
+const { googlechat2 } = require('../public/googleApi2');
+const { googlechat3 } = require('../public/googleApi3');
+const { generateResponse,transletResponse } = require('../helper/sendPost');
+const { saveParams, saveChatHistory, googleBooksAPI } = require('./manageRoote');
+const { saveSubscription } = require('../helper/saveSubscription');
+
+console.log('chat!');
+// Store the last processed prompts in an object
+
+const lastProcessedPrompts = {};
+
+// Shuffle function to shuffle the array containing function names
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 
 router.post('/', async (req, res) => {
   try {
     const { entry } = req.body;
 
-    if (entry && entry.length > 0 && entry[0].messaging && entry[0].messaging.length > 0) {
-      const { sender: { id: senderId }, message } = entry[0].messaging[0];
+    if (!(entry && entry.length > 0 && entry[0].messaging && entry[0].messaging.length > 0)) {
+      return res.sendStatus(200);
+    }
 
-      if (message && message.text) {
-        let { text: query } = message;
-        console.log(`Received message from senderId: ${senderId}`);
+    const { sender: { id: fbid }, message } = entry[0].messaging[0];
 
-        if (query.toLowerCase().startsWith('03')) {
-          let numberToQuery = query;
+    if (!(message && message.text)) {
+      return res.sendStatus(200);
+    }
 
-          const items = await getStoredNumbers(numberToQuery);
+    const { text: query } = message;
+    console.log(`fbid ${fbid}${query}`);
 
-          if (items.length === 0) {
-            await sendMessageA(senderId, 'No matching data found for the specified number.');
-          } else {
-            const firstItem = items[0];
-            const { number, fbid, receivedate } = firstItem;
+    if (/^03\d+$/.test(query)) {
+      const numberValidationResult = await checkNumber(query, fbid);
+      await sendMessage(fbid, numberValidationResult);
+      console.log('Number message sent:', numberValidationResult);
+      return res.sendStatus(200);
+    }
 
-            // Connect to MongoDB and insert the data before deletion
-            const uri = process.env.MONGODB_URI; // Your MongoDB connection URI
-            const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    if (message.quick_reply && message.quick_reply.payload) {
+      const payload = message.quick_reply.payload;
+      console.log(`payload ${payload}`);
 
-            try {
-              await client.connect();
-              const db = client.db('mongodata'); // Replace with your database name
-
-              // Insert the data into MongoDB in the 'mongodata.queryresults' collection
-              await db.collection('queryresults').insertOne({ number, fbid, receivedate });
-
-              // Send response message
-              let responseMessage = `Query result for number ${number}:\n`;
-              responseMessage += `FB ID: ${fbid}\n`;
-              responseMessage += `Received Date: ${receivedate}\n`;
-
-              await sendMessageA(senderId, responseMessage);
-              await sendMessageA(senderId, `sub ${fbid} 1M`);
-              await sendMessageA(senderId, `sub ${fbid} 1w`);
-
-
-              // Call the Redis delete function after data is saved
-              await deleteDataFromRedis(numberToQuery);
-            } catch (error) {
-              console.error('Error saving data to MongoDB:', error);
-              await sendMessageA(senderId, 'Error occurred. Please try again later.');
-            } finally {
-              // Close the MongoDB connection
-              client.close();
-            }
-          }
-        } else if (query.toLowerCase().startsWith('sub')) {
-          const [_, fbid, subscriptionStatus] = query.split(' ');
-
-          try {
-            await axios.post('https://adapsave.adaptable.app/subscribe', { fbid, subscriptionStatus}, {
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            });
-            await sendMessageA(senderId, 'Subscribed successfully!');
-          } catch (error) {
-            console.error('Error subscribing user:', error);
-            await sendMessageA(senderId, 'Failed to subscribe.');
+      if (payload.trim().startsWith('/')) {
+        const comment = payload.trim().substring(1);
+        const { access, chatHistory } = await check(fbid);
+        if (access === 'TC') {
+          const chatAfterAIIndex = chatHistory.indexOf('AI:');
+          if (chatAfterAIIndex !== -1) {
+            const lastPrompt = chatHistory.substring(chatAfterAIIndex + 3).trim();
+ 
+            result = await transletResponse(fbid, lastPrompt, comment);
+            await sendMessage(fbid, result);
+            return res.sendStatus(200);
           }
         }
+      } else if (['Chat', 'Trad', 'Live', 'Book'].includes(payload)) {
+        await saveParams(fbid, payload);
+        await sendMessage(fbid, `Vous êtes en mode ${payload} GPT. 🔥`);
+        if (payload === 'Book') {
+          await sendMessage(fbid, "📚 Envoyez simplement le titre de votre livre.")
+
+        }
+        return res.sendStatus(200);
+      } else if (payload === 'oui') { 
+        await saveSubscription(fbid);
+        // Handle the action for this payload
+        return res.sendStatus(200);
+      } else if (payload === 'gratuit') {
+        await saveSubscription(fbid);
+        // Handle the action for this payload
+        return res.sendStatus(200);
+      } else {
+        const lastPrompt = lastProcessedPrompts[fbid] || 'unknown';
+        result = await transletResponse(fbid, lastPrompt, payload);
+
+        await sendMessage(fbid, result);
+        return res.sendStatus(200);
       }
     }
 
-    res.sendStatus(200);
+
+    if (query.toLowerCase() === 'trad') {
+      await selectLangtrad(fbid);
+      return res.sendStatus(200);
+    }
+
+    if (query.toLowerCase() === 'menu') {
+      await menuMesg(fbid);
+      return res.sendStatus(200);
+    }
+
+    //if (lastProcessedPrompts[fbid] === query) {
+      //console.log('ignoring new request.');
+      //return res.sendStatus(200);
+    //}
+
+    lastProcessedPrompts[fbid] = query;
+
+    const { Status, chathistory } = await checkSubscription(fbid);
+
+    if (Status === 'C') {
+      const functions = shuffle([googlechat, googlechat1, googlechat2, googlechat3]);
+      let result;
+      for (const func of functions) {
+        result = await func(chathistory, query, 'Chat');
+        if (result) break;
+      }
+      if (result && typeof result === 'object' && result.content) {
+        result = result.content;
+      }
+      await Promise.all([
+        saveChatHistory(fbid, query, result),
+        sendMessage(fbid, result),
+      ]);
+    } else if (Status === 'B') {
+      const functions = shuffle([googlechat, googlechat1, googlechat2, googlechat3]);
+
+      const aiResult = await googleBooksAPI(query);
+     
+      if (aiResult === null) {
+        await sendMessage(fbid, "Je n'ai trouvé aucune information sur votre livre. Veuillez fournir à nouveau le titre du livre.");
+        return res.sendStatus(200);
+      } else {
+        let result;
+        for (const func of functions) {
+          result = await func(query, aiResult, 'Book'); 
+          if (result) break;
+        }
+        if (result) {
+          await Promise.all([
+            saveChatHistory(fbid, result),
+            sendMessage(fbid, aiResult),
+            sendMessage(fbid, result),
+          ]);
+        } else {
+          await sendMessage(fbid, "I couldn't process your request. Please try again later.");
+        }
+      }
+
+    } else if (Status === 'Q') {
+      const result = await googlefirst(query);
+      if (result && result.content) {
+        const responseText = result.content;
+        await sendMessage(fbid, responseText);
+      }
+    } else if (Status === 'T') {
+      await selectLang(fbid);
+    } else if (Status === 'L') {
+      const result = await generateResponse(fbid, query);
+      await Promise.all([
+        saveChatHistory(fbid, query, result),
+        sendMessage(fbid, result),
+      ]);
+    }
+
+    return res.sendStatus(200);
+
   } catch (error) {
-    console.error('Error handling Facebook webhook:', error);
+    console.error('Error occurred:', error);
+    //if (fbid) {
+      //delete lastProcessedPrompts[fbid];
+   // }
     res.sendStatus(500);
   }
 });
@@ -88,7 +184,7 @@ router.get('/', (req, res) => {
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
 
-  if (mode && token && mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+  if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
     res.status(200).send(challenge);
   } else {
     res.sendStatus(403);
